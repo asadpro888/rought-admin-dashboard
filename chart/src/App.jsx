6b9b5a1bf864f5e70cde3f2a8c412282348ee082
Пользователь <asadpro888>
2024-08-03 12:34:48 +0300

    admin

---
+++ chart/src/App.jsx
@@ -0,0 +1,30 @@
+import PieColor from './PieChart'
+import PrimarySearchAppBar from './AppBar'
+import StraightAnglePieChart from './Chart'
+import ScaleExample from './Line';
+import BasicComposition from './Axios'
+import BasicLegend from './Legend'
+import BasicColor from './Styling'
+import BiaxialLineChart from './LineChart'
+import ElementHighlights from './Zoom'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <PrimarySearchAppBar />
+      <PieColor />
+      <StraightAnglePieChart />
+      <ScaleExample />
+      <BasicComposition />
+      <BasicLegend />
+      <BasicColor />
+      <BiaxialLineChart />
+      <ElementHighlights />
+    </>
+  )
+}
+
+export default App
